c1973d3582e2caed3928b6a668fbcc74a6a86ce5
Shoaib Rain <rainshoaib2013@outlook.com>
2022-01-28 02:44:23 -0600

    Jsonparsing project started

--- class/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ByTwos b1 = new ByTwos();
-        b1.printStuff();
-    }
-
-}
---
+++ jsonparsing/src/main/java/com/shoaibr/jsonparsing/Json.java
@@ -0,0 +1,5 @@
+package com.shoaibr.jsonparsing;
+
+public class Json {
+
+}
